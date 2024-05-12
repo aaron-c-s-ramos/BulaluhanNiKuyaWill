a8001cc7b22b21671c7f3e89a25b1e5901a6efbc
Aaron Christopher Ramos <aaron.c.s.ramos@gmail.com>
2024-05-13 00:42:38 +0800

    Implemented Downloading of Error Messages as Image

--- conn.php
+++ conn.php
@@ -8,55 +8,73 @@ $dbName = "id22124684_bulaluhan_ni_kuya_will"; // The name of the database to co
 
 try
 {
-    // Attempt to create a new PDO instance for database connection
-    $conn = new PDO("mysql:host=$servername;port=$port;dbname=$dbName", $username, $password);
-    
-    // Set the error mode to throw exceptions for errors
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-    // Set the default fetch mode to return results as associative arrays
-    $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-    
-    // Disable emulation of prepared statements if the driver supports them
-    $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+  // Attempt to create a new PDO instance for database connection
+  $conn = new PDO("mysql:host=$servername;port=$port;dbname=$dbName", $username, $password);
+  
+  // Set the error mode to throw exceptions for errors
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  
+  // Set the default fetch mode to return results as associative arrays
+  $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+  
+  // Disable emulation of prepared statements if the driver supports them
+  $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 }
 catch (Exception $e)
 {
-    // If there's an error during connection, display the error message and stack trace
-    echo '
-        <html>
-        <head>
-        </head>
-        <body>
-            <script type="text/javascript">
-                document.addEventListener("DOMContentLoaded", function(){
-                    var errorModal = new bootstrap.Modal(document.getElementById(\'errorModal\'), {});
-                    errorModal.show();
-                });
-            </script>
-            <div class="modal fade" id="errorModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="errorModalLabel">
-                <div class="modal-dialog modal-xl modal-dialog-centered modal-dialog-scrollable">
-                    <div class="modal-content">
-                        <div class="modal-header d-flex justify-content-center">
-                            <svg class="bi flex-shrink-0 me-2" width="1.5rem" height="1.5rem" role="img" aria-label="Danger:" viewBox="0 0 16 16">
-                                <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-                            </svg>
-                            <h1 class="modal-title fs-3" id="errorModalLabel">Error: Connection Failed</h1>
-                        </div>
-                        <div class="modal-body fs-5">
-                            <span>';
-                                echo $e->getMessage().'<br>';
-                                echo '<hr>';
-                                echo $e->getTraceAsString().'<br>';
-                                echo '
-                            </span>
-                        </div>
-                        <div class="modal-footer justify-content-evenly fs-5">';
-                            echo '<span>Please save this message and contact the developer.</span>
-                        </div>
-                    </div>
+  // If there's an error during connection, display the error message and stack trace
+  echo '
+      <html>
+      <head>
+      </head>
+      <body>
+      <!-- Script to show an error modal when the page loads -->
+        <script type="text/javascript">
+            document.addEventListener("DOMContentLoaded", function()
+            {
+              // Create a new Bootstrap modal instance for error display
+              var errorModal = new bootstrap.Modal(document.getElementById(\'errorModal\'), {});
+              // Show the error modal immediately
+              errorModal.show();
+            });
+        </script>
+        <!-- Error Modal -->
+        <div class="modal fade" id="errorModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="errorModalLabel">
+          <div class="modal-dialog modal-xl modal-dialog-centered modal-dialog-scrollable">
+            <div class="modal-content">
+            <!-- Container for the screenshot content -->
+              <div id="forScreenshot">
+                <div class="modal-header d-flex justify-content-center">
+                  <svg class="bi flex-shrink-0 me-2" width="1.5rem" height="1.5rem" role="img" aria-label="Danger:" viewBox="0 0 16 16">
+                    <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                  </svg>
+                  <h1 class="modal-title fs-3" id="errorModalLabel">Error: Connection Failed</h1>
                 </div>
+                <div class="modal-body fs-5">
+                <!-- Error details to be displayed -->
+                  <span>
+                    <!-- PHP code to echo SQL error and stack trace -->';
+                    echo $e->getMessage() . '<br>';
+                    echo '<hr>';
+                    echo $e->getTraceAsString() . '<br>';
+                    echo '
+                  </span>
+                </div>
+              </div>
+              <div class="modal-footer justify-content-evenly fs-5">
+              <!-- Modal footer with error button for downloading error details -->';
+                echo '  <span class="center-all">
+                            Please save this message and contact the developer.<br>
+                            <button class="btn btn-lg btn-secondary text-nowrap mt-3 mb-2" onclick="downloadError(\'forScreenshot\')">Download Message</button>
+                        </span>
+              </div>
             </div>
-        </body>
-        </html>';
+          </div>
+        </div>
+        <!-- Script to capture the screenshot -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js" integrity="sha512-BNaRQnYJYiPSqHHDb58B0yaPfCu+Wgds8Gp/gU33kqBtgNS4tSPHuGibyoeqMV/TJlSKda6FXzoEyYGjTe+vXA==" crossorigin="anonymous" referrerpolicy="no-referrer" defer></script>
+        <!-- Script for downloading error details -->
+        <script src="downloadError.js" defer></script>
+      </body>
+      </html>';
 }
--- searchResponse.php
+++ searchResponse.php
@@ -2,73 +2,90 @@
 // Check if the request method is POST and the submit button is set
 if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["submit"]))
 {
-    // Clean the input to prevent SQL injection
-    $search = clean_input($_POST['search']);
-
-    try
+  // Clean the input to prevent SQL injection
+  $search = clean_input($_POST['search']);
+  try
+  {
+    // Prepare SQL query to select reservation by ID
+    $sql = "SELECT * FROM current_reservations WHERE Reservation_ID = :Reservation_ID";
+    $stmt = $conn->prepare($sql);
+    // Bind the parameter to the prepared statement
+    $stmt->bindParam(':Reservation_ID', $search, PDO::PARAM_INT);
+    // Execute the query
+    $stmt->execute();
+    // Check if any rows were returned
+    if ($stmt->rowCount() > 0)
     {
-        // Prepare SQL query to select reservation by ID
-        $sql = "SELECT * FROM current_reservations WHERE Reservation_ID = :Reservation_ID";
-        $stmt = $conn->prepare($sql);
-        // Bind the parameter to the prepared statement
-        $stmt->bindParam(':Reservation_ID', $search, PDO::PARAM_INT);
-        // Execute the query
-        $stmt->execute();
-        // Check if any rows were returned
-        if ($stmt->rowCount() > 0)
-        {
-            // Fetch the first row as an associative array
-            $row = $stmt->fetch(PDO::FETCH_ASSOC);
-            // Check if the fetched row is an array
-            if (is_array($row))
-            {
-                // Assign the Reservation_ID to a variable
-                $Reservation_ID = $row['Reservation_ID'];
-            }
-        }
-        else
-        {
-            // Set Reservation_ID to -1 if no match found
-            $Reservation_ID = -1;
-        }
+      // Fetch the first row as an associative array
+      $row = $stmt->fetch(PDO::FETCH_ASSOC);
+      // Check if the fetched row is an array
+      if (is_array($row))
+      {
+        // Assign the Reservation_ID to a variable
+        $Reservation_ID = $row['Reservation_ID'];
+      }
     }
-    catch (Exception $e)
+    else
     {
-        // Output an error modal with details of the exception
-        echo '
-        <html>
-        <head>
-        </head>
-        <body>
-            <script type="text/javascript">
-                document.addEventListener("DOMContentLoaded", function(){
-                    var errorModal = new bootstrap.Modal(document.getElementById(\'errorModal\'), {});
-                    errorModal.show();
-                });
-            </script>
-            <div class="modal fade" id="errorModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="errorModalLabel">
-                <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-                    <div class="modal-content">
-                        <div class="modal-header d-flex justify-content-center">
-                            <h1 class="modal-title fs-3" id="errorModalLabel">Error: Connection Failed</h1>
-                        </div>
-                        <div class="modal-body fs-5">
-                            <span>';
-                                echo $sql. '<br>'. $e->getMessage().'<br>';
-                                echo '<hr>';
-                                echo $sql. '<br>'. $e->getTraceAsString().'<br>';
-                                echo '
-                            </span>
-                        </div>
-                        <div class="modal-footer justify-content-evenly fs-5">';
-                            echo '<span>Please save this message and contact the developer.</span>
-                        </div>
-                    </div>
-                </div>
+      // Set Reservation_ID to -1 if no match found
+      $Reservation_ID = -1;
+    }
+  }
+  catch (Exception $e)
+  {
+    // Output an error modal with details of the exception
+    echo '
+    <html>
+    <head>
+    </head>
+    <body>
+    <!-- Script to show an error modal when the page loads -->
+      <script type="text/javascript">
+        document.addEventListener("DOMContentLoaded", function()
+        {
+          // Create a new Bootstrap modal instance for error display
+          var errorModal = new bootstrap.Modal(document.getElementById(\'errorModal\'), {});
+          // Show the error modal immediately
+          errorModal.show();
+        });
+      </script>
+      <!-- Error Modal -->
+      <div class="modal fade" id="errorModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="errorModalLabel">
+        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+          <div class="modal-content">
+          <!-- Container for the screenshot content -->
+            <div id="forScreenshot">
+              <div class="modal-header d-flex justify-content-center">
+                <h1 class="modal-title fs-3" id="errorModalLabel">Error: Connection Failed</h1>
+              </div>
+              <div class="modal-body fs-5">
+              <!-- Error details to be displayed -->
+                <span>
+                  <!-- PHP code to echo SQL error and stack trace -->';
+                  echo $sql. '<br>'. $e->getMessage().'<br>';
+                  echo '<hr>';
+                  echo $sql. '<br>'. $e->getTraceAsString().'<br>';
+                  echo '
+                </span>
+              </div>
             </div>
-        </body>
-        </html>';
-    }
-    // Close the database connection
-    $conn = null;
+            <div class="modal-footer justify-content-evenly fs-5">
+            <!-- Modal footer with error button for downloading error details -->';
+              echo '  <span class="center-all">
+                          Please save this message and contact the developer.<br>
+                          <button class="btn btn-lg btn-secondary text-nowrap mt-3 mb-2" onclick="downloadError(\'forScreenshot\')">Download Message</button>
+                      </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Script to capture the screenshot -->
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js" integrity="sha512-BNaRQnYJYiPSqHHDb58B0yaPfCu+Wgds8Gp/gU33kqBtgNS4tSPHuGibyoeqMV/TJlSKda6FXzoEyYGjTe+vXA==" crossorigin="anonymous" referrerpolicy="no-referrer" defer></script>
+      <!-- Script for downloading error details -->
+      <script src="downloadError.js" defer></script>
+    </body>
+    </html>';
+  }
+  // Close the database connection
+  $conn = null;
 }
